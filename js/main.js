4b06a3630b5c2b15f1022a6f012768fb17689d34
Seth A. Roby <sroby@uci.edu>
2017-05-01 16:14:04 -0700

    Initial commit
    I was [reading an article]( https://www.vox.com/policy-and-politics/2017/5/1/15493088/donald-trumps-absurd-100th-day-week-was-his-presidency-in-miniature) and it noted, offhandedly:
    
    > One hundred days isn’t even 7 percent of Trump’s term.
    
    I wondered, immediately, "what percentage is it?" And a quick trip to my calculator found out (6.84%). Then I started wondering how to *feel* that number a bit more viscerally. This is the result.

---
+++ js/main.js
@@ -0,0 +1,59 @@
+var startTS = Date.UTC(2017, 0, 20, 7, 0, 0);
+var endTS = Date.UTC(2021, 0, 20, 7, 0, 0);
+
+function numberWithCommas(n) {
+    var parts=n.toString().split(".");
+    return parts[0].replace(/\B(?=(\d{3})+(?!\d))/g, ",") + (parts[1] ? "." + parts[1] : "");
+}
+
+function addTooltips() {
+  addTooltip($('#milliseconds.its-been'), 'millisecond', 1);
+  addTooltip($('#seconds.its-been'), 'second', 1000);
+  addTooltip($('#minutes.its-been'), 'minute', 1000*60);
+  addTooltip($('#hours.its-been'), 'hour', 1000*60*60);
+  addTooltip($('#days.its-been'), 'day', 1000*60*60*24);
+  addTooltip($('#weeks.its-been'), 'week', 1000*60*60*24*7);
+  addTooltip($('#years.its-been'), 'year', 1000*60*60*24*365.251);
+}
+
+function addTooltip(itsBeen, label, slice) {
+  var msTotal = endTS - startTS;
+  itsBeen.attr('title', `Each ${label} is ${(slice/msTotal*100.0).toFixed(9)}% of the term`);
+}
+
+function updateItsBeen(itsBeen, msElapsed, msTotal, divider) {
+  itsBeen.find('.elapsed .val').text(numberWithCommas((msElapsed/divider).toFixed(0)));
+  itsBeen.find('.to-endure .val').text(numberWithCommas(((msTotal-msElapsed)/divider).toFixed(0)));
+}
+
+function updateTimes() {
+  var now = new Date();
+  var nowTS = now.getTime();
+  
+  var msTotal = endTS - startTS;
+  var msElapsed = nowTS - startTS;
+  var msPercent = (msElapsed/msTotal*100.0);
+  
+  updateItsBeen($('#milliseconds.its-been'), msElapsed, msTotal, 1);
+  updateItsBeen($('#seconds.its-been'), msElapsed, msTotal, 1000);
+  updateItsBeen($('#minutes.its-been'), msElapsed, msTotal, 1000*60);
+  updateItsBeen($('#hours.its-been'), msElapsed, msTotal, 1000*60*60);
+  updateItsBeen($('#days.its-been'), msElapsed, msTotal, 1000*60*60*24);
+  updateItsBeen($('#weeks.its-been'), msElapsed, msTotal, 1000*60*60*24*7);
+  updateItsBeen($('#years.its-been'), msElapsed, msTotal, 1000*60*60*24*365.251);
+  
+  $('.percentage').text(`We're ${msPercent.toFixed(5)}% of the way through`);
+  $('#content').attr('style', `background-position: 50% ${100+msPercent}%;`);
+}
+
+function animateTimes() {
+  requestAnimationFrame(function() {
+    updateTimes();
+    animateTimes();
+  });
+}
+
+$(function() {
+  addTooltips();
+  animateTimes();
+});
